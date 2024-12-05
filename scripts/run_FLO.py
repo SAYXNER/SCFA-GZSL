d8715ed728e1d1cce2b9ac8af548a747bff4046b
SAYXNER <166737357+SAYXNER@users.noreply.github.com>
2024-12-05 17:02:29 +0800

    Update run_FLO.py

--- scripts/run_FLO.py
+++ scripts/run_FLO.py
@@ -4,7 +4,7 @@ import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 #os.environ['OMP_NUM_THREADS'] = '4'
-os.system('''python E:\GITHUB\cVAE_cons测\\train_no_contrast.py --dataset FLO_EPGN --ga 5 --nSample 1500 --gpu 0 \
+os.system('''python ...\\train.py --dataset FLO_EPGN --ga 5 --nSample 1500 --gpu 0 \
   --lr 0.0001  --classifier_lr 0.005 --kl_warmup 0.01 --gen_nepoch 800 \
-  --vae_dec_drop 0.4 --vae_enc_drop 0.4 --ae_drop 0.2''') #contrast
+  --vae_dec_drop 0.4 --vae_enc_drop 0.4 --ae_drop 0.2''') 
 
